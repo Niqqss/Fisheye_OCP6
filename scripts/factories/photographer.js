b66b3dabeec99ed530538ac02bbe30689af855a1
Nicolas Stefanelly <nstefanelly@gmail.com>
2023-01-24 16:34:33 +0100

    index HTML content

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -4,16 +4,27 @@ function photographerFactory(data) {
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        let href = './photographer.html?id=' + id;
+        const article = document.createElement('article');
 
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        article.innerHTML =
+            `<a href = "#">
+                <img src="${picture}" alt="${name}">
+                <h2>${name}</h2>
+            </a>
+            <div>
+                <p>${city}, ${country}</p>
+                <p>${tagline}</p>
+                <p>${price}€/jour</p>
+            </div>`;
+
+        // const img = document.createElement('img');
+        // img.setAttribute("src", picture)
+        // const h2 = document.createElement('h2');
+        // h2.textContent = name;
+        // article.appendChild(img);
+        // article.appendChild(h2);
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return {getUserCardDOM};
 }
